103741ecbf4c19aee6ab33f79e41d2f3a6e58751
MyApple <myapple@MyAppletekiMacBook-Pro.local>
2015-11-11 14:23:30 +0800

    Initial Commit

---
+++ MyTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyTest
+//
+//  Created by MyApple on 11/11/15.
+//  Copyright (c) 2015 MyApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyTest
+//
+//  Created by MyApple on 11/11/15.
+//  Copyright (c) 2015 MyApple. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
